6508bd6bb97cea307c2d178bfae80680f277eede
Rose Wan <rwan@mail.com>
2017-07-15 19:35:06 +0000

    getting search page results to show but still working on it

--- farmers-market/app/controllers/search_controller.rb
+++ farmers-market/app/controllers/search_controller.rb
@@ -1,5 +1,12 @@
 class SearchController < ApplicationController
   def index
     puts "I CAME TO INDEX WITH PARAMETERS"
+    # @product = Product.find(:all, :conditions => ['product_name LIKE ?',  "%#{params[:search]}%"])
+    puts 'RESULTS'
+    @product = Product.find_by product_name: params[:search]
   end
 end
+
+# <% @product.each do |p| %>
+#   <h3><%= p.inspect %></h3>
+# <% end %>
