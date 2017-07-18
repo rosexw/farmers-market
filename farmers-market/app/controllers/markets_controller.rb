d9c94e7a326b63583afd1ade3fd80e3081922288
kamir222 <katia.amir@gmail.com>
2017-07-18 22:30:59 +0000

    add google maps to /markets

--- farmers-market/app/controllers/markets_controller.rb
+++ farmers-market/app/controllers/markets_controller.rb
@@ -2,11 +2,18 @@ class MarketsController < ApplicationController
 
   def index
     @market = Market.all
+    @hash = Gmaps4rails.build_markers(@market) do |market, marker|
+      marker.lat market.lat
+      marker.lng market.long
+      marker.infowindow market.description
+    end
+
   end
 
   def show
     @market = Market.find params[:id]
-    puts @market.inspect
+
+
   end
 
 end
