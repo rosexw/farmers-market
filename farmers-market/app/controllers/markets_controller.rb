1fb3a81763725f766ca60ed57b64a64f07434746
Rose Wan <rwan@mail.com>
2017-07-20 19:50:16 +0000

    env keys saved. Remember to fix market 423.

--- farmers-market/app/controllers/markets_controller.rb
+++ farmers-market/app/controllers/markets_controller.rb
@@ -7,13 +7,11 @@ class MarketsController < ApplicationController
       marker.lng market.long
       marker.infowindow market.description
     end
-
   end
 
   def show
     @market = Market.find params[:id]
     @products = Product.find_product_by_market_name(@market.name)
-
     @hash = Gmaps4rails.build_markers(@market) do |market, marker|
       marker.lat market.lat
       marker.lng market.long
