1f8f765affe12650a3076659434d546052c8612b
kamir222 <katia.amir@gmail.com>
2017-07-20 23:43:10 +0000

    insert farmer participating in each farmers market

--- farmers-market/app/models/market.rb
+++ farmers-market/app/models/market.rb
@@ -1,6 +1,7 @@
 class Market < ApplicationRecord
   mount_uploader :image, MarketImageUploader
   has_many :farmer_markets
+  has_many :farmers, through: :farmer_markets
 
   def self.find_markets_by_product_name(name)
     Market.joins(farmer_markets: [farmer: [farmers_products: [ :product]]]).where(products: {product_name: name.downcase}).distinct
