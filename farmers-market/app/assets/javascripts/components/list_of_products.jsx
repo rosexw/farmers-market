1f7f54ea9968b89396328b26a1126c039bb07967
Meagan Blais <meaganblais@gmail.com>
2017-07-24 16:33:21 -0400

    farmer can see all their product on dashboard

--- farmers-market/app/assets/javascripts/components/list_of_products.jsx
+++ farmers-market/app/assets/javascripts/components/list_of_products.jsx
@@ -2,13 +2,33 @@ class Products extends React.Component {
 
   render() {
   const listProducts = this.props.products.map((product, i) =>
-  <div>
-    <a href={'/products/' + product.id} key={i}>{product.name}</a>
-  </div>
+    <div>
+      <tbody>
+        <tr>
+          <td><b>{product.product_name}</b></td>
+          <td>{product.product_type}</td>
+          <td>{product.organic.toString()}</td>
+          <td>{product.non_gmo.toString()}</td>
+          <td>{product.on_hand.toString()}</td>
+        </tr>
+      </tbody>
+    </div>
   );
    return (
-     <div>{listProducts}</div>
+     <div>
+       <table>
+         <thead>
+           <tr>
+             <th>Products</th>
+             <th>Type</th>
+             <th>Organic</th>
+             <th>Non-GMO</th>
+             <th>On-Hand</th>
+           </tr>
+         </thead>
+          {listProducts}
+      </table>
+    </div>
    );
   }
-
 }
--- farmers-market/app/models/farmers_product.rb
+++ farmers-market/app/models/farmers_product.rb
@@ -3,4 +3,13 @@ class FarmersProduct < ApplicationRecord
   belongs_to :product
 
   scope :farmer_products, -> (id) { where(farmer_id: id) }
+
+  def self.get_products_farmer_offers(farmer_id)
+    array = []
+    all_product_ids = FarmersProduct.where(farmer_id: farmer_id)
+    all_product_ids.each do |f_product|
+      array.push Product.find_by(id: f_product.product_id)
+    end
+    return array
+  end
 end
