aa6df41317207795d638a31a05a018e28e9a826a
Rose Wan <rwan@mail.com>
2017-07-24 02:30:19 +0000

    basic style changes to Fred the Farmer's dashboard such as boxing the info to the left, fred's picture is a circle, and padding border spaces are set

--- farmers-market/app/assets/javascripts/components/farmerProfile.jsx
+++ farmers-market/app/assets/javascripts/components/farmerProfile.jsx
@@ -21,14 +21,16 @@ class FarmerProfile extends React.Component {
 
   render() {
     return (<div>
-      <div className="field"> Farm Name: {this.props.farm_name} </div>
-      <div className="field"> First Name: {this.props.first_name} </div>
-      <div className="field"> Last Name: {this.props.last_name} </div>
-      <div className="field"> Email: {this.props.email} </div>
-      <div className="field"> Phone No: {this.props.phone_no} </div>
-      <div className="field"> Website_Link: {this.props.website_link} </div>
-      <div className="field"> Description: {this.props.description} </div>
-      <div><img src={this.props.image}/></div>
+      <div className="farmer-profile-image"><img src={this.props.image}/></div>
+      <div className="farmer-profile-info">
+        <div className="field"><b> Farm Name: </b>{this.props.farm_name} </div>
+        <div className="field"><b> First Name: </b>{this.props.first_name} </div>
+        <div className="field"><b> Last Name: </b>{this.props.last_name} </div>
+        <div className="field"><b> Email: </b>{this.props.email} </div>
+        <div className="field"><b> Phone No: </b>{this.props.phone_no} </div>
+        <div className="field"><b> Website_Link: </b>{this.props.website_link} </div>
+        <div className="field"><b> Description: </b>{this.props.description} </div>
+      </div>
       <button onClick={this.handleClickAddProduct}>Add Product</button>
       <button onClick={this.handleClickAddMarket}>Add Market</button>
       <button onClick={this.handleClickViewProduct.bind(this)}>View Products</button>
